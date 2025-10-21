97d9c2c64cf15d37df1b22abae8e4145ae234694
astik123-tech <astiksarathe575@gmail.com>
2025-10-21 23:36:19 +0530

    Code Clean-up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmiCalculatorModule } from './emi-calculator/emi-calculator.module';
 import { ElectricityBillModule } from './electricity-bill/electricity-bill.module';
 import { HttpModule } from '@nestjs/axios';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EmiCalculatorModule, ElectricityBillModule, AuthModule, HttpModule],
+  imports: [ElectricityBillModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/emi-calculator/emi-calculator.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmiCalculatorService } from './emi-calculator.service';
-import { EmiCalculatorController } from './emi-calculator.controller';
-
-@Module({
-  controllers: [EmiCalculatorController],
-  providers: [EmiCalculatorService],
-})
-export class EmiCalculatorModule {}
