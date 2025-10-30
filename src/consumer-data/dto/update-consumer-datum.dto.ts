cc4869272c0740dced4c1fabdac29ec683114c82
astik123-tech <astiksarathe575@gmail.com>
2025-10-30 12:12:59 +0530

    Deleted consumer data mpdule replace by solar project

--- src/consumer-data/dto/update-consumer-datum.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateConsumerDatumDto } from './create-consumer-datum.dto';
-
-export class UpdateConsumerDatumDto extends PartialType(CreateConsumerDatumDto) {}
--- src/consumer-data/entities/consumer-datum.entity.ts
+++
@@ -1,6 +0,0 @@
-// Re-export the comprehensive schema from the schemas directory
-export type { ConsumerDataDocument as ConsumerDatumDocument } from '../../schemas/consumer-data.schema';
-export {
-  ConsumerData as ConsumerDatum,
-  ConsumerDataSchema as ConsumerDatumSchema,
-} from '../../schemas/consumer-data.schema';
